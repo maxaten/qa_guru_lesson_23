996f3fbf2626784f54cae108167b357a977d1087
Maksim Atenyaev <atenjaev@gmail.com>
2023-07-10 22:02:29 +0600

    HW

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -19,9 +19,9 @@ def test_search_1():
         ).should(have.size_greater_than(0))
 
 
-@allure.step("Open GitHub wiki")
+@allure.step("Open Google wiki")
 def test_search_2():
-    with step('Type search "GitHub"'):
+    with step('Type search "Google"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
             'Google'
